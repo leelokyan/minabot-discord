9a160c27b0854174c0ed7f6c888ed30ff051f221
Neuton Foo <neuton.foo@gmail.com>
2021-01-13 17:42:46 -0800

    Initial commit

---
+++ index.js
@@ -0,0 +1,48 @@
+require("dotenv").config();
+
+const axios = require("axios");
+const instance = axios.create({
+  baseURL: "https://api.tenor.com/v1/",
+  timeout: 1000,
+  params: {
+    key: process.env.TENOR_API_KEY,
+    locale: "en_US",
+    contentfilter: "off",
+    media_filter: "basic",
+    ar_range: "all",
+  },
+});
+
+async function getTenorGif(query) {
+  return instance
+    .get("/random", {
+      params: {
+        q: query,
+        limit: 1,
+      },
+    })
+    .then(response => response.data.results[0].url);
+}
+
+const Discord = require("discord.js");
+const client = new Discord.Client();
+
+client.login(process.env.DISCORD_TOKEN);
+
+client.on("ready", async () => {
+  console.log(`Logged in as ${client.user.tag}!`);
+});
+
+client.on("message", async msg => {
+  if (msg.channel.id === process.env.DISCORD_CHANNEL_ID) {
+    if (msg.content === "!twice") {
+      msg.channel.send(await getTenorGif("twice"));
+    } else if (msg.content === "!mina") {
+      msg.channel.send(await getTenorGif("mina"));
+    } else if (msg.content === "!bts") {
+      msg.channel.send(await getTenorGif("nct"));
+    } else if (msg.content === "!nct") {
+      msg.channel.send(await getTenorGif("bts"));
+    }
+  }
+});
