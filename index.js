fba1b9f1f90d2b357415727abffaf42d0733813f
Neuton Foo <neuton.foo@gmail.com>
2021-01-13 17:46:11 -0800

    Changed dotenv

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
+// require("dotenv").config();
 
 const axios = require("axios");
 const instance = axios.create({
